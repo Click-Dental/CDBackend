d0f9a00ab49388b8e703df61c14f32e4d1588c51
KarolZerna <karol.zern@gmail.com>
2022-11-30 01:46:09 -0400

    roles

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 app.use('/api/pacientes', require('./routes/paciente'));
 app.use('/api/dentistas', require('./routes/dentista'));
+app.use('/api', require('./routes/user'));
 
 app.listen(4000, () => {
     console.log('El servidor esta corriendo perfectamente')
